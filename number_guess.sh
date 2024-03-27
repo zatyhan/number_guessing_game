91516e3a33a83e3c79c655e6f3d8e8caede1975f
Nur Eizzaty Hamzaid <nureizzaty@gmail.com>
2024-03-27 00:36:53 +0000

    feat: retrieve/add user info

--- number_guess.sh
+++ number_guess.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
 
-PSQL="psql --username=freecodecamp --dbname=<database_name> -t --no-align -c"
+PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
+
+echo Enter your username: 
+read username
+
+USER_EXIST=$($PSQL "SELECT * FROM user_activity WHERE username='$username'")
+if [[ -z $USER_EXIST ]]
+then 
+  INSERT_USER=$($PSQL "INSERT INTO user_activity(username) VALUES ('$username')")
+  echo "Welcome, $username! It looks like this is your first time here."
+else 
+  games_played=$($PSQL "SELECT games_played FROM user_activity WHERE username='$username'")
+  best_game=$($PSQL "SELECT best_game FROM user_activity WHERE username='$username'")
+  echo "Welcome back, $username! You have played $games_played games, and your best game took $best_game guesses."
+fi
